05cc6ef011c0e5da7a0e2d6ac57d228a00a04373
Nacher Uddin Sakib <23101093@uap-bd.edu>
2025-09-28 23:39:12 +0600

    Update views.py delete user

--- user/views.py
+++ user/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.decorators import login_required
+from .models import CollabUser
+from .forms import UserUpdateForm, CollabUserUpdateForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+# delete user
+@login_required
+def delete_user(request):
+    if request.method == "POST":
+        password = request.POST.get("password")
+        user = request.user
+
+        if user.check_password(password):
+            username = user.username
+
+            logout(request)
+            user.delete()
+
+            messages.success(request, f"User '{username}' account deleted successfully.")
+            return redirect('home')
+        else:
+            messages.error(request, "Incorrect password. Please try again.")
+
+    return render(request, "user/delete_confirmation.html")
