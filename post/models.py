61f90bd071e387310ac16e486ed006ff8bab2bef
Afrin Akter Lima <135718471+afrin0537@users.noreply.github.com>
2025-10-11 03:16:35 +0600

    Update models.py of post

--- post/models.py
+++ post/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from collaboration.models import Collaborator
+from django.utils import timezone
 
-# Create your models here.
+class Event(models.Model):
+    organizer = models.ForeignKey(Collaborator, on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    status = models.CharField(max_length=100)
+    cover_pic = models.ImageField(upload_to="photos/", null=True, blank=True)
+    schedule = models.ImageField(upload_to="photos/", null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Project(models.Model):
+    organizer = models.ForeignKey(Collaborator, on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateField()
+    deadline = models.DateField()
+    status = models.CharField(max_length=100)
+    cover_pic = models.ImageField(upload_to="photos/", null=True, blank=True)
+    criteria = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Thesis(models.Model):
+    organizer = models.ForeignKey(Collaborator, on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    research_topic = models.CharField(max_length=100)
+    research_field = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=100)
+    cover_pic = models.ImageField(upload_to="photos/", null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
