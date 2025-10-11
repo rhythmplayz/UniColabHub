2158c12a515d402218842a4d7c3d015bb7a0d31a
nafeesarafat <168644214+nafeesarafat@users.noreply.github.com>
2025-10-11 17:25:35 +0600

    Update views.py of post
    creation of event, project and thesis post

--- post/views.py
+++ post/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProjectForm,EventForm,ThesisForm
+from .models import Project,Event,Thesis
+from collaboration.models import Collaborator
 
-# Create your views here.
+def create_event(request):
+    if request.method == "POST":
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            collaborator = Collaborator(user=request.user, post=None)
+            collaborator.save()
+            event.organizer = collaborator
+            event.save()
+            collaborator.post = event
+            collaborator.save()
+            return redirect("home")
+    else:
+        form = EventForm()
+    return render(request,'post/create_event.html',{"form":form})
+
+def create_thesis(request):
+    if request.method == "POST":
+        form = ThesisForm(request.POST)
+        if form.is_valid():
+            thesis = form.save(commit=False)
+            collaborator = Collaborator(user=request.user, post=None)
+            collaborator.save()
+            thesis.organizer = collaborator
+            thesis.save()
+            collaborator.post = thesis
+            collaborator.save()
+            return redirect("home")
+    else:
+        form = ThesisForm()
+    return render(request,'post/create_thesis.html',{"form":form})
+
+def create_project(request):
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            project = form.save(commit=False)
+            collaborator = Collaborator(user=request.user, post=None)
+            collaborator.save()
+            project.organizer = collaborator
+            project.save()
+            collaborator.post = project
+            collaborator.save()
+            return redirect("home")
+
+    else:
+        form = ProjectForm()
+
+    return render(request,'post/create_project.html',{"form":form})
