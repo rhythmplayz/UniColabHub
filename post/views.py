7a5d8d8965771b30cf45047937fc8e054f336f33
nafeesarafat <168644214+nafeesarafat@users.noreply.github.com>
2025-10-22 03:23:33 +0600

    Update views.py for post

--- post/views.py
+++ post/views.py
@@ -52,3 +52,72 @@ def create_project(request):
         form = ProjectForm()
 
     return render(request,'post/create_project.html',{"form":form})
+
+def view_project(request,pk):
+    project = Project.objects.get(pk=pk)
+    return render(request,'post/view_project.html',{"project":project})
+
+def view_thesis(request,pk):
+    thesis = Thesis.objects.get(pk=pk)
+    return render(request,'post/view_thesis.html',{"thesis":thesis})
+
+def view_event(request,pk):
+    event = Event.objects.get(pk=pk)
+    return render(request,'post/view_event.html',{"event":event})
+
+def edit_project(request,pk):
+    project = Project.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ProjectForm(request.POST,instance=project)
+        if form.is_valid():
+            project = form.save(commit=False)
+            return redirect("home")
+    else:
+        form = ProjectForm(instance=project)
+    return render(request,'post/edit_project.html',{"form":form})
+
+def edit_thesis(request,pk):
+    thesis = Thesis.objects.get(pk=pk)
+    if request.method == "POST":
+        form = ThesisForm(request.POST,instance=thesis)
+        if form.is_valid():
+            thesis = form.save(commit=False)
+            return redirect("home")
+    else:
+        form = ThesisForm(instance=thesis)
+
+    return render(request,'post/edit_thesis.html',{"form":form})
+
+def edit_event(request,pk):
+    event = Event.objects.get(pk=pk)
+    if request.method == "POST":
+        form = EventForm(request.POST,instance=event)
+        if form.is_valid():
+            event = form.save(commit=False)
+            return redirect("home")
+
+    else:
+        form = EventForm(instance=event)
+
+    return render(request,'post/edit_event.html',{"form":form})
+
+def delete_project(request,pk):
+    project = Project.objects.get(pk=pk)
+    if request.method == "POST":
+        project.delete()
+        return redirect("home")
+    return render(request,'post/delete_project.html',{"project":project})
+
+def delete_thesis(request,pk):
+    thesis = Thesis.objects.get(pk=pk)
+    if request.method == "POST":
+        thesis.delete()
+        return redirect("home")
+    return render(request,'post/delete_thesis.html',{"thesis":thesis})
+
+def delete_event(request,pk):
+    event = Event.objects.get(pk=pk)
+    if request.method == "POST":
+        event.delete()
+        return redirect("home")
+    return render(request,'post/delete_event.html',{"event":event})
