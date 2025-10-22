7b597c2880906912cf601cab3df6342baff3022c
Nacher Uddin Sakib <23101093@uap-bd.edu>
2025-10-22 14:15:20 +0600

    Add Notification and JoinRequest models

--- collaboration/models.py
+++ collaboration/models.py
@@ -1,7 +1,10 @@
+
 from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.utils import timezone
+
 
 class Collaborator(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
@@ -22,6 +25,7 @@ class Resource(models.Model):
     amount = models.PositiveIntegerField()
     university = models.CharField(max_length=200)
     department = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="photos/", null=True, blank=True, default=None)
 
     content_type = models.ForeignKey(ContentType, on_delete=models.SET_NULL, null=True)
     object_id = models.PositiveIntegerField(null=True, blank=True)
@@ -29,3 +33,22 @@ class Resource(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    message = models.TextField()
+    read_status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.user.username + ": " + self.message
+
+class JoinRequest(models.Model):
+    organizer = models.ForeignKey(Collaborator, on_delete=models.SET_NULL, null=True)
+    recipient = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    notification = models.ForeignKey(Notification, on_delete=models.SET_NULL, null=True)
+    approve_status = models.CharField(max_length=120, default="not approved")
+
+    def __str__(self):
+        return self.organizer.user.username + ": " + self.recipient.username + ": " + self.approve_status
