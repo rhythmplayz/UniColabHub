15c674e51a6a7ac228ddd73caa515e19f1722634
Nacher Uddin Sakib <23101093@uap-bd.edu>
2025-10-22 14:15:43 +0600

    Register JoinRequest and Notification models in admin

--- collaboration/admin.py
+++ collaboration/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Collaborator,Resource
+from .models import Collaborator,Resource,JoinRequest,Notification
 
 admin.site.register(Collaborator)
 admin.site.register(Resource)
+admin.site.register(JoinRequest)
+admin.site.register(Notification)
